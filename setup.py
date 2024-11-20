60d05a7df67ce2d344aab9587e41818c5edb6564
GweiDart <btnewcomer@pm.me>
2024-11-20 00:47:20 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="localtunnel",
-    version='0.1.1',
+    version='{{VERSION_PLACEHOLDER}}',
     description="A Python client for Localtunnel.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
